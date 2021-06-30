b3a934f5d37347ce4544470752a6a7b6d245aa18
William <jorge.laba@we-help.site>
2021-06-30 09:37:20 -0300

    Construct basic proof of work.

--- poc/testbytes.go
+++ poc/testbytes.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"math"
+	"math/big"
 )
 
 func main() {
+	fmt.Println(math.MaxInt64)
 
-	b1 := []byte("Ola pequeno mundo bytes")
+	target := big.NewInt(1)
+	fmt.Println(target)
+	fmt.Println(uint(256 - 10))
+	target.Lsh(target, uint(256-10))
+
+	fmt.Println(target)
+
+	/*b1 := []byte("Ola pequeno mundo bytes")
 	b2 := []byte("Ola pequeno mundo 2")
 
 	f := bytes.Join([][]byte{b1, b2}, []byte{})
@@ -15,7 +24,7 @@ func main() {
 	fmt.Println(b1)
 	fmt.Println(b2)
 	fmt.Println(f[:])
-
+	*/
 	//fmt.Println(reflect.TypeOf([]byte(teste)))
 	//info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 }
